80be0357178f583825f55dd2b6d371437fa44589
Carlos Ramirez <carlos@extrasistemas.com>
2022-01-25 00:23:38 -0600

    added component completed task with css style and add storybook test

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,32 @@
-import React from 'react'
+// src/components/Task.js
 
-const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) =>{
-    return (
-        <div className="list-item">
-            <input type="text" value={title} readOnly={true} />
-        </div>
-    )
-}
+import React from "react";
 
-export {Task}
+const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+      </div>
+
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export { Task };
